611e7acc403037f9f0307963934a4ee298f94c15
fantasyzernontra <fantasyzernontra@hotmail.com>
2021-07-23 12:53:40 +0700

    :sparkles: Making code compatible with Persist Storage

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ import {AppRegistry} from 'react-native';
 import React from 'react';
 import App from './App';
 import {name as appName} from './app.json';
+
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 // Redux instance
-import configureStore from './store';
+import {store, persistor} from './store';
 
 // Ignite Redux instance
-const store = configureStore();
 
 const RNRedux = () => (
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
--- store/index.js
+++ store/index.js
@@ -1,13 +1,22 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistStore, persistReducer} from 'redux-persist';
+
 import {createStore, combineReducers} from 'redux';
 
 import countReducer from './reducers/countReducer';
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
 const rootReducer = combineReducers({
-  count: countReducer,
+  count: persistReducer(persistConfig, countReducer),
 });
 
 const configureStore = () => {
   return createStore(rootReducer);
 };
 
-export default configureStore;
+export const store = configureStore();
+export const persistor = persistStore(store);
